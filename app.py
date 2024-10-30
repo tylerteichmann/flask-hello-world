73543313688f189426e44fa71ca615a34ff89885
tylerteichmann <tyler.j.teichmann@gmail.com>
2024-10-29 19:29:54 -0600

    added db_create route

--- app.py
+++ app.py
@@ -13,15 +13,18 @@ def testing():
     return "Database Connection Successful"
 
 @app.route("/db_create")
-def createing():
+def creating():
     conn = psycopg2.connect("postgresql://db_hello_world_user:T5nfjWI8ihyWIjN8Gi0T4wkii4xdniEg@dpg-csgni788fa8c7390lbeg-a/db_hello_world")
     cur = conn.cursor()
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS Basketball(
+        CREATE TABLE IF NOT EXISTS Basketball (
                 First varchar(255),
                 Last varchar(255),
                 City varchar(255),
                 Name varchar(255),
                 Number int
             );
-""")
+    """)
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
