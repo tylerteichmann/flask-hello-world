ed318f8ad47078649efa31a119bd2f7b0bf4401e
tylerteichmann <tyler.j.teichmann@gmail.com>
2024-10-29 19:34:06 -0600

    added db_insert route

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World from Tyler Teichmann in 3308'
+
 
 @app.route("/db_test")
 def testing():
     conn = psycopg2.connect("postgresql://db_hello_world_user:T5nfjWI8ihyWIjN8Gi0T4wkii4xdniEg@dpg-csgni788fa8c7390lbeg-a/db_hello_world")
     conn.close()
     return "Database Connection Successful"
+
 
 @app.route("/db_create")
 def creating():
@@ -28,3 +31,20 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+
+@app.route("/db_insert")
+def inserting():
+    conn = psycopg2.connect("postgresql://db_hello_world_user:T5nfjWI8ihyWIjN8Gi0T4wkii4xdniEg@dpg-csgni788fa8c7390lbeg-a/db_hello_world")
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    """)
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
